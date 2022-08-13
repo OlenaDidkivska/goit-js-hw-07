a8b68afd2838947379b72f4b1991cd4f5b68010d
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-08-13 23:04:46 +0300

    add tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const imgMarcup = addGalleryItems(galleryItems);
+
+function addGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function galleryItemsHendler(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img src="${
+      event.target.closest("img").dataset.source
+    }" width="800" height="600">`
+  );
+
+  console.log(event.target);
+  console.log(modal.show());
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarcup);
+galleryContainer.addEventListener("click", galleryItemsHendler);
+
+document.addEventListener("keydown", (evt) => {
+  const pushEscape = evt.code === "Escape";
+  const modalOn = document.querySelector(".basicLightbox");
+  if (!modalOn) {
+    return;
+  }
+  if (pushEscape) {
+    modalOn.remove();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const imgMarcup = addGalleryItems(galleryItems);
+
+function addGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+function galleryItemsHendler(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a");
+
+  gallery.on("show.simplelightbox", function () {
+    gallery.captionType = 250;
+  });
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", imgMarcup);
+galleryContainer.addEventListener("click", galleryItemsHendler);
